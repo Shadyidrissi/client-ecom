1c3115505f038a7c49eaad38ce433158f15ff2cd
Shady Idrissi <chadiidac@gmail.com>
2024-09-14 21:47:02 +0100

    page product

---
+++ src/app/[id]/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='product-details'>
+      <div id='image-show-div'></div>
+      <div id='details-show-div'> </div>
+    </div>
+  )
+}
+
+export default page
